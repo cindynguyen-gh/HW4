4db9b1d0f35b931bd9a9df9f3a1bc8b3d995cd10
Cindy Nguyen <cindynguyen@Cindys-MacBook-Pro.local>
2019-11-12 18:30:31 -0800

    Initial commit

---
+++ build.py
@@ -0,0 +1,11 @@
+def main():
+    # Your original SSG code from last homework goes here, indented. # For example:
+    top = open("templates/top.html").read()
+    # etc..
+    # And at the bottom of the same file:
+    if __name__ == "__main__":
+    main()
+
+# And at the bottom of the same file:
+if __name__ == "__main__":
+    main()    
