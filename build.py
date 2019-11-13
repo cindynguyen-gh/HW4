626507eddb352d9e37aa87ab775e82b10ded1cbe
Cindy Nguyen <cindynguyen@Cindys-MacBook-Pro.local>
2019-11-12 18:35:16 -0800

    setup base.py file

--- build.py
+++ build.py
@@ -1,11 +1,7 @@
 def main():
-    # Your original SSG code from last homework goes here, indented. # For example:
-    top = open("templates/top.html").read()
-    # etc..
-    # And at the bottom of the same file:
-    if __name__ == "__main__":
-    main()
+    base = open("templates/base.html").read()
+    
 
-# And at the bottom of the same file:
+
 if __name__ == "__main__":
     main()    
