c205f346140c2e29fce2a11c47a61b400d4e601e
Cindy Nguyen <cindynguyen@Cindys-MacBook-Pro.local>
2019-11-19 18:30:27 -0800

    begins putting code into loop; gets content_file variable access working

--- build.py
+++ build.py
@@ -1,5 +1,5 @@
-def main():
-    base = open("templates/base.html").read()
+# def main():
+#     base = open("templates/base.html").read()
     
 
 pages = [
@@ -19,48 +19,54 @@ pages = [
     }
 ]
 
-if __name__ == "__main__":
-    main()    
+# if __name__ == "__main__":
+#     main()    
+
+# Read in the entire template
+template = open("./templates/base.html").read()
 
 for page in pages:
     print("page:",page['title']) # Replace this line eventually with other stuff...
+    content_file = page['filename']
+
+    index_content = open(content_file).read()
+    finished_index_page = template.replace("{{content}}", index_content)
+    open(output_file, "w+").write(finished_index_page)
 
 # Where "page" is a dictionary with a key "title"
 page_title = page['title']
 # print(page_title)
 
 
-# Read in the entire template
-template = open("./templates/base.html").read()
 
 # Read in the content of the index HTML page
-index_content = open("content/index.html").read()
 
-projects_content = open("content/projects.html").read()
 
-contact_content = open("content/contact.html").read()
 
 # Use the string replace
+index_content = open("content/index.html").read()
 finished_index_page = template.replace("{{content}}", index_content)
 open("docs/index.html", "w+").write(finished_index_page)
 
+projects_content = open("content/projects.html").read()
 finished_projects_page = template.replace("{{content}}", projects_content)
 open("docs/projects.html", "w+").write(finished_projects_page)
 
+contact_content = open("content/contact.html").read()
 finished_contact_page = template.replace("{{content}}", contact_content)
 open("docs/contact.html", "w+").write(finished_contact_page)
 
 # def apply_template(content):
 # TODO: Read in template, do string replacing, and return result return results
 
-def main():
-    content = open('docs/index.html').read()
-    resulting_html_for_index = apply_template(content)
+# def main():
+#     content = open('docs/index.html').read()
+#     resulting_html_for_index = apply_template(content)
 
-def main():
-    content = open('docs/projects.html').read()
-    resulting_html_for_projects = apply_template(content)
+# def main():
+#     content = open('docs/projects.html').read()
+#     resulting_html_for_projects = apply_template(content)
 
-def main():
-    content = open('docs/contact.html').read()
-    resulting_html_for_contact = apply_template(content)
+# def main():
+#     content = open('docs/contact.html').read()
+#     resulting_html_for_contact = apply_template(content)
